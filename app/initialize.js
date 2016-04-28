787594ea8768ee5c3a160d6136f87580a5ad098b
rattk <rattk@users.noreply.github.com>
2016-04-29 00:38:24 +0800

    Add result section

--- app/initialize.js
+++ app/initialize.js
@@ -6,15 +6,17 @@ import $ from 'jquery'
 import 'select2'
 
 const motions = {}
+const memberVotes = {}
 
-const updateSelect = (rootNode, $motionSelect) => (event) => {
+const onSelectChange = (rootNode, $motionSelect) => (event) => {
   const selectedMotions = _.map($motionSelect.val(), (motionId) => {
     return [motionId, motions[motionId]]
   })
-  console.log(selectedMotions)
+  // console.log(selectedMotions)
   render((
-    <form className="container list-group">
+    <form className="container">
       <h3>議案投票</h3>
+      <div className="list-group">
       {_.map(selectedMotions, ([motionId, motion], i) => {
         return (
           <div key={i} className="form-group list-group-item">
@@ -31,7 +33,23 @@ const updateSelect = (rootNode, $motionSelect) => (event) => {
           </div>
         )
       })}
+      </div>
     </form>
+  ), rootNode)
+}
+
+const onVoteChange = (rootNode) => (event) => {
+  const $form = $(event.target).closest('form')
+  const voted = $form.serializeArray()
+  // console.log(voted)
+
+  render((
+    <div className="container">
+      <h3>結果</h3>
+      <div className="list-group">
+
+      </div>
+    </div>
   ), rootNode)
 }
 
@@ -40,14 +58,16 @@ document.addEventListener('DOMContentLoaded', () => {
   const $motionCount = $('<div>載入議案資料中⋯⋯</div>').appendTo($app)
   const $motionSelect = $('<select name="select-items" multiple style="width: 100%" />')
   const $votes = $('<div />')
+  const $result = $('<div />')
 
-  $motionSelect.on('change', updateSelect($votes.get(0), $motionSelect))
+  $motionSelect.on('change', onSelectChange($votes.get(0), $motionSelect))
+  $votes.on('change', 'input', onVoteChange($result.get(0)))
 
   let motionCount = 0
   $.getJSON('data/votes.json', (votes) => {
     // TODO generate data as simple array
     votes = _.flatten(_.map(votes, (v) => v))
-    console.log(votes)
+    // console.log(votes)
 
     const groups = {}
     _.each(votes, (meeting) => {
@@ -80,5 +100,10 @@ document.addEventListener('DOMContentLoaded', () => {
 
     $motionCount.text(`共 ${motionCount} 個議案`)
     $votes.appendTo($app)
+    $result.appendTo($app)
+
+    if (__DEV__) {
+      $motionSelect.val('Council Meeting_17/10/2012_1').trigger('change')
+    }
   })
 })
